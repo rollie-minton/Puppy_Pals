83c3d25c656f119822724197878d7631f1ebe1c7
rollie <rollieminton@outlook.com>
2024-02-13 17:48:57 -0500

    No puppies :(

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-//import { useState } from 'react'
+import { useState } from 'react'
 //import viteLogo from '/vite.svg'
 //import './App.css'
+import {puppyList} from './data.js'
+
+const [puppies, setPuppies] = useState(puppyList);
+
+console.log(puppies);
 
 
   //return (
     //<>
-      <div>
+      <div>{
+        puppies.map((puppy) => {
+          return <p>{puppy.name}</p>
+        })
+        }
       </div>
       
    // </>
- // )
+  //)
 
-
-//export default App
